0d2e785ad32187ade8ef9a7d9576074dfb3e1312
PrathmL <prathmeshlandge74@gmail.com>
2026-03-31 10:30:45 +0530

    Frontend Updates

--- src/main/java/com/esspbackend/config/WebConfig.java
+++ src/main/java/com/esspbackend/config/WebConfig.java
@@ -27,11 +27,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
         Path uploadDir = Paths.get(dirName);
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-        
+        String resourceLocation = uploadDir.toUri().toString();
+
         if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-        
+
         registry.addResourceHandler("/" + dirName + "/**")
-                .addResourceLocations("file:/" + uploadPath + "/");
+                .addResourceLocations(resourceLocation);
     }
 }
